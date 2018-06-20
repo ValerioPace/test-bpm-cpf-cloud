13c222e71be310883aadc926b6cedcd4936ee94f
Pace Valerio <v.pace@almaviva.it>
2018-06-20 17:41:09 +0200

    commit change log4j/slj4j implementation

--- test-bpm-cpf-client/src/main/java/it/gov/mef/cloudify/servlet/NoiPAServletInitializer.java
+++ test-bpm-cpf-client/src/main/java/it/gov/mef/cloudify/servlet/NoiPAServletInitializer.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.ClassPathResource;
