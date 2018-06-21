5140e510017e45036e51ae9e4fd0ee89a69075f8
Pace Valerio <v.pace@almaviva.it>
2018-06-21 11:25:06 +0200

    commit define Spring data JPA JNDI data source and emf environment

--- test-bpm-cpf-client/src/main/java/it/gov/mef/cloudify/servlet/NoiPAServletInitializer.java
+++ test-bpm-cpf-client/src/main/java/it/gov/mef/cloudify/servlet/NoiPAServletInitializer.java
@@ -2,6 +2,9 @@ package it.gov.mef.cloudify.servlet;
 
 import java.io.IOException;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -12,7 +15,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import it.gov.mef.cloudify.ServiceController;
 import it.gov.mef.cloudify.ServiceType;
@@ -21,11 +30,15 @@ import it.gov.mef.cloudify.kie.bpm.BPMClient;
 import it.gov.mef.cloudify.kie.ruleengine.RuleEngineClient;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class})
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @ComponentScan(basePackageClasses = {ServiceController.class, BPMClient.class, RuleEngineClient.class})
 public class NoiPAServletInitializer extends SpringBootServletInitializer {
  
+	@Value("${spring.datasource.jndi-name}")
+	private String primaryJndiName;
+    
+    @Value("${spring.datasource.driver-class}")
+    private String driverClass;
 	 
     @Bean
     public static PropertyPlaceholderConfigurer ppc() throws IOException {
@@ -40,6 +53,31 @@ public class NoiPAServletInitializer extends SpringBootServletInitializer {
     	
     	KieServiceManagerDelegate serviceManager = new KieServiceManagerDelegate(ServiceType.HTTPS, "https", System.getProperty("HOSTNAME"), "8443");
         return serviceManager;
+    }
+    
+    @Primary
+    @Bean(destroyMethod = "", name="dataSource")
+    public DataSource jndiDataSource() {
+    	JndiDataSourceLookup lookup = new JndiDataSourceLookup();
+    	return lookup.getDataSource(primaryJndiName);  	
+    }
+    
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
+        lef.setDataSource(dataSource);
+        lef.setJpaVendorAdapter(jpaVendorAdapter);
+        lef.setPackagesToScan("it.gov.mef.cloudify.process.types");
+        return lef;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setShowSql(false);
+        hibernateJpaVendorAdapter.setGenerateDdl(true);
+        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+        return hibernateJpaVendorAdapter;
     }
    
 	
