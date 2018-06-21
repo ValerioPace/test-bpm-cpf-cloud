0ce26e4fb9b97cfbd018d1198387080d5ade6bca
Pace Valerio <v.pace@almaviva.it>
2018-06-21 10:46:57 +0200

    commit remove @component annotation

--- test-bpm-cpf-client/src/main/java/it/gov/mef/cloudify/kie/KieServiceManagerDelegate.java
+++ test-bpm-cpf-client/src/main/java/it/gov/mef/cloudify/kie/KieServiceManagerDelegate.java
@@ -31,7 +31,6 @@ import org.springframework.stereotype.Component;
 
 import it.gov.mef.cloudify.ServiceType;
 
-@Component("kieServiceManager")
 public class KieServiceManagerDelegate {
 
 	private KieCommands commands;
